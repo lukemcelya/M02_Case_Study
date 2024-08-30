cb6b9972795c27d6a5a8d4a731b90e1188261c59
lukemcelya <johnlukemcelya@gmail.com>
2024-08-30 18:04:26 -0500

    main branch

---
+++ gpa_check.py
@@ -0,0 +1,15 @@
+import sys
+
+last_name = str(input("Enter student last name or 'ZZZ' to quit: "))
+ 
+if last_name == 'ZZZ':
+    print("Done processing student records.")
+    sys.exit()
+
+first_name = str(input("Student first name: "))
+gpa = float(input("Student GPA: "))
+
+if gpa >= 3.5:
+    print(first_name, last_name, "has made the Dean's List!")
+elif gpa >= 3.25:
+    print(first_name, last_name, "has made the Honor Roll!")
