d8aeb9d2076946bc5614755dc7e23eb5b86fdf98
Luke M. <144291635+lukemcelya@users.noreply.github.com>
2024-09-01 15:08:09 -0500

    gpa_check.py

--- gpa_check.py
+++ gpa_check.py
@@ -1,15 +1,17 @@
-import sys
+while True:
 
-last_name = str(input("Enter student last name or 'ZZZ' to quit: "))
+    last_name = str(input("Enter student last name or 'ZZZ' to quit: "))
  
-if last_name == 'ZZZ':
-    print("Done processing student records.")
-    sys.exit()
+    if last_name.upper() == 'ZZZ':
+        print("Done processing student records.")
+        break
 
-first_name = str(input("Student first name: "))
-gpa = float(input("Student GPA: "))
+    first_name = str(input("Student first name: "))
+    gpa = float(input("Student GPA: "))
 
-if gpa >= 3.5:
-    print(first_name, last_name, "has made the Dean's List!")
-elif gpa >= 3.25:
-    print(first_name, last_name, "has made the Honor Roll!")
+    if gpa >= 3.5:
+        print(first_name, last_name, "has made the Dean's List!")
+    elif gpa >= 3.25:
+        print(first_name, last_name, "has made the Honor Roll!")
+
+print("Goodbye :)")
